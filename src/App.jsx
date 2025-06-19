e0bb36dfd757cf1fe3af5bde4511f063a999699a
gabrieldreamz <gabrieldreamx@gmail.com>
2025-06-19 09:13:15 +0100

    modified README.md

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,6 @@ const App = () => {
 
       <div className="wrapper">
         <header>
-          {/* <img src="./hero.png" alt="Hero Banner" /> */}
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
